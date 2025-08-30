dcbd81cf17001a95c428e7127b36544759df57d2
hjun813 <hyeonjun0899@gmail.com>
2025-08-30 15:12:19 +0900

    Add: 그리디_hjun

---
+++ Week02/LiveCoding/BOJ_1138_한 줄로 서기/hjun.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N = int(input())
+left = list(map(int, input().split()))
+answer = deque([])
+
+for i in range(N):
+    index = N - 1 - i
+    temp = []
+    if index == N:
+        answer.append(N)
+        continue
+        
+    leftNum = left[index]
+    
+    for j in range(i+1):
+        if j == leftNum:
+            temp.append(index+1)
+            continue
+        temp.append(answer.popleft())
+        
+    for j in temp:
+        answer.append(j)
+        
+print(*answer)
+        
